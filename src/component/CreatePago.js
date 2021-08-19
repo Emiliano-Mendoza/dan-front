a506876df89fc6a9681d98eb97fa23a649a11302
Emiliano Mendoza <eemmiii96@gmail.com>
2021-08-19 11:27:44 -0300

    pagos casi terminado

--- src/component/CreatePago.js
+++ src/component/CreatePago.js
@@ -23,7 +23,20 @@ export default class CreatePago extends Component {
         clienteObra: '',
 
         form: {
+            type: 'efectivo',
+            observacion: '',
+            nroRecibo: null,
+            cbuOrigen: null,
+            cbuDestino: null,
+            codigoTransferencia: null,
+            nroCheque: null,
+            banco: null
         },
+
+        pago:{
+            cliente: {id: null},
+            medio: {},
+        }
     }
 
     async componentDidMount() {
@@ -32,6 +45,13 @@ export default class CreatePago extends Component {
         } else {
             this.getClient();
             this.getPagos();
+
+            this.setState({
+                pago:{
+                    ...this.state.pago,
+                    cliente: {id: this.state.client.id}
+                }
+            })
         }
     }
 
@@ -42,7 +62,70 @@ export default class CreatePago extends Component {
     async getPagos() {
         const res = await axios.get('http://localhost:7000/ms-cuentacorriente/api/pago/cliente/' + cookies.get('id'));
         await this.setState({ pagos: res.data });
-        console.log(this.state.pagos);
+    }
+
+    tipoChange = async (e) => {
+        await this.setState({
+            form:{
+                ...this.state.form,
+                type: e.target.value
+            }
+        });
+        console.log(this.state.form.type)
+    }
+
+    handleChange = (e) =>{
+        this.setState({
+            form:{
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        });
+        // console.log(e.target.value + "   " + e.target.name);
+    }
+
+    formarMedioPago(){
+        if(this.state.form.type === 'efectivo'){
+            this.setState({form: {type: 'efectivo', observacion: this.state.form.observacion, nroRecibo: this.state.form.nroRecibo}})
+        }else if(this.state.form.type === 'transferencia'){
+            this.setState({form: {type: 'transferencia', observacion: this.state.form.observacion, cbuOrigen: this.state.form.cbuOrigen,
+                cbuDestino: this.state.form.cbuDestino, codigoTransferencia: this.state.form.codigoTransferencia}});
+        }else if(this.state.form.type === 'cheque'){
+            this.setState({form: {type: 'cheque', observacion: this.state.form.observacion, nroCheque: this.state.form.nroCheque,
+                banco: this.state.form.banco}});
+        }
+    }
+
+    registrarPago = async (e) => {
+        await this.formarMedioPago();
+
+        await this.setState({
+            pago:{
+                cliente: {id: this.state.client.id},
+                medio: this.state.form
+            }
+        });
+        console.log(this.state.pago);
+        await axios.post('http://localhost:7000/ms-cuentacorriente/api/pago', this.state.pago);
+
+        this.getPagos();
+        await this.setState({
+            form: {
+                type: 'efectivo',
+                observacion: '',
+                nroRecibo: null,
+                cbuOrigen: null,
+                cbuDestino: null,
+                codigoTransferencia: null,
+                nroCheque: null,
+                banco: null
+            },
+        })
+    }
+
+    removerPago = async (id) =>{
+        await axios.delete("http://localhost:7000/ms-cuentacorriente/api/pago/id/" + id);
+        this.getPagos();
     }
 
     render() {
@@ -61,11 +144,11 @@ export default class CreatePago extends Component {
                             {pago.medio.codigoTransferencia ? <p>Código de transferencia: {pago.medio.codigoTransferencia}</p> : null}
                             {pago.medio.nroCheque ? <p>Número de cheque: {pago.medio.nroCheque}</p> : null}
                             {pago.medio.banco ? <p>Banco: {pago.medio.banco}</p> : null}
-                            <button type="button" className="deletebtn btn btn-danger">Remover</button>
+                            <button type="button" className="deletebtn btn btn-danger" onClick={()=>this.removerPago(pago.id)}>Remover</button>
                             <p>Fecha: {pago.fechaPago}</p>
                         </div>
                     ))}
-                    <button type="button" className="agregar-obra-button btn btn-primary">Agregar</button>
+
                 </div>
             </div>
             <div className="col-md-4">
@@ -73,40 +156,52 @@ export default class CreatePago extends Component {
                     <h3>Realizar Pago:</h3>
                     <form>
                         <div className="form-group">
-                            <h6>Razón Social:</h6>
-                            <input type='text' className="form-control" required />
-                            <h6>Cuit:</h6>
-                            <input type='text' className="form-control" required />
-                            <h6>E-mail:</h6>
-                            <input type='email' className="form-control" required />
+                            <h6>Medio de pago:</h6>
+                            <select id="selector" name="type" onChange={this.tipoChange} className="input-pago">
+                                    <option value="efectivo">Efectivo</option>
+                                    <option value="transferencia">Transferencia</option>
+                                    <option value="cheque">Cheque</option>
+                            </select>
+                            <h6>Observacion:</h6>
+                            <input type='text' className="form-control input-pago" name="observacion" onChange={this.handleChange}/>
+                            {this.state.form.type === 'efectivo' ? 
+                                <div>
+                                    <h6>Número de recibo:</h6>
+                                    <input type='number' className="form-control input-pago" name="nroRecibo" onChange={this.handleChange}  />
+                                </div> : null}
 
-                            <div>
-                                <h4 className="obras">Obras:</h4>
-                                <p>Descripcion:</p>
-                                <input type='text' className="form-control" />
-                                <p>Tipo de obra: </p>
-                                <select id="selector">
-                                    <option > - </option>
-                                    <option value="Reforma" >Reforma</option>
-                                    <option value="Casa">Casa</option>
-                                    <option value="Edificio">Edificio</option>
-                                    <option value="Vial">Vial</option>
-                                </select>
-                                <p>Latitud: </p>
-                                <input type="number" step="0.1" className="form-control" />
-                                <p>Longitud: </p>
-                                <input type="number" step="0.1" className="form-control" />
-                                <p>Direccion:</p>
-                                <input type='text' className="form-control" />
-                                <p>Superficie: </p>
-                                <input type="number" step="1" className="form-control" />
-                            </div>
-                            <button type="button" className="btn-primary" id="agregar" >
-                                Agregar Obra
-                            </button>
+                            {this.state.form.type === 'transferencia' ? 
+                                <div>
+                                    <h6>CBU Origen:</h6>
+                                    <input type='text' className="form-control input-pago" name="cbuOrigen" onChange={this.handleChange}  />
+                                </div> : null}
+
+                            {this.state.form.type === 'transferencia' ? 
+                                <div>
+                                    <h6>CBU Destino:</h6>
+                                    <input type='text' className="form-control input-pago" name="cbuDestino" onChange={this.handleChange} />
+                                </div> : null}  
+
+                            {this.state.form.type === 'transferencia' ? 
+                                <div>
+                                    <h6>Código de transferencia:</h6>
+                                    <input type='number' className="form-control input-pago" name="codigoTransferencia" onChange={this.handleChange}  />
+                                </div> : null}
+
+                            {this.state.form.type === 'cheque' ? 
+                                <div>
+                                    <h6>Número de cheque:</h6>
+                                    <input type='text' className="form-control input-pago" name="nroCheque" onChange={this.handleChange}  />
+                                </div> : null}
+
+                            {this.state.form.type === 'cheque' ? 
+                                <div>
+                                    <h6>Banco:</h6>
+                                    <input type='text' className="form-control input-pago"  name="banco" onChange={this.handleChange}/>
+                                </div> : null}
                         </div>
-                        <button type="submit" className="btn btn-primary" id="guardar" >
-                            Guardar
+                        <button type="submit" className="btn btn-primary agregar-obra-button" id="registrar" onClick={()=>this.registrarPago()}>
+                            Registrar
                         </button>
                     </form>
                 </div>
